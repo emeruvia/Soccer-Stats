0923769c1ef36c9ab26068ecd609410e005e90f8
Edgar Meruvia Garron <emeruvia@gmail.com>
2018-10-30 11:29:37 -0400

    networks utilities for retrofit enabled

--- app/src/main/java/emg/soccerstats/interfaces/RetrofitService.kt
+++ app/src/main/java/emg/soccerstats/interfaces/RetrofitService.kt
@@ -1,4 +1,13 @@
 package emg.soccerstats.interfaces
 
+import emg.soccerstats.models.CompetitionsModel
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RetrofitService {
+  @GET("/V2/competitions")
+  fun competitionsService(
+    @Query("X-Auth-Token") authToken: String
+  ): Call<CompetitionsModel>
 }
---
+++ app/src/main/java/emg/soccerstats/utils/RetrofitClient.kt
@@ -0,0 +1,21 @@
+package emg.soccerstats.utils
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+  private var retrofit: Retrofit? = null
+
+  fun buildClient(baseUrl: String): Retrofit {
+    if (retrofit == null) {
+      retrofit = Retrofit.Builder()
+          .baseUrl(baseUrl)
+          .addConverterFactory(GsonConverterFactory.create())
+          .build()
+    }
+    return retrofit!!
+  }
+  companion object {
+    val baseUrl: String = "http://api.football-data.org"
+  }
+}
