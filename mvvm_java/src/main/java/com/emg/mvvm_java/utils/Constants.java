15daa983927c9a4ebe6955c752b136702b4384bf
Edgar Meruvia <emeruvia@gmail.com>
2019-05-24 00:40:00 -0400

    Finished basic implementation for retrofit request of the competition list

--- mvvm_java/src/main/java/com/emg/mvvm_java/utils/Constants.java
+++ mvvm_java/src/main/java/com/emg/mvvm_java/utils/Constants.java
@@ -4,5 +4,6 @@ package com.emg.mvvm_java.utils;
  * Created by emeruvia on 5/22/2019.
  */
 public class Constants {
-  public final String BASE_URL = "https://api.football-data.org";
+  public static final String BASE_URL = "https://api.football-data.org";
+  public static final int NETWORK_TIMEOUT = 3000;
 }
