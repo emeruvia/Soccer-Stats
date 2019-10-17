51fe34e717d868aa01712b8799c332801c401740
Edgar Meruvia <emeruvia@gmail.com>
2019-10-16 23:58:15 -0400

    Include: - Timber dependency - Coroutine dependency - Update kotlin dependency - BaseApplication class (SoccerApplication)

---
+++ app/src/main/java/emg/soccerstats/SoccerApplication.kt
@@ -0,0 +1,14 @@
+package emg.soccerstats
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class SoccerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+    }
+
+}
